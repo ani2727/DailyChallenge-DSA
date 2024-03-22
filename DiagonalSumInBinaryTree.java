6e40c5a54cbba927ca710fd4e3e2376fb2be1246
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-03-22 15:59:45 +0530

    Create DiagonalSumInBinaryTree.java

---
+++ DiagonalSumInBinaryTree.java
@@ -0,0 +1,30 @@
+//Problem Link: https://rb.gy/6u6gm7
+
+//code
+
+class Tree {
+    public static ArrayList <Integer> diagonalSum(Node root) 
+    {
+        ArrayList<Integer> ans  = new ArrayList<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            int sum = 0;
+            for(int i = 0 ; i < size ; i ++)
+            {
+                Node curr = q.poll();
+                while(curr != null)
+                {
+                    if(curr.left != null) q.add(curr.left);
+                    
+                    sum += curr.data;
+                    curr = curr.right;
+                }
+            }
+            ans.add(sum);
+        }
+        return ans;
+    }
+}
