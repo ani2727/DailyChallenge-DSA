ade89bed64379305f64646fbb73138c588afc332
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-03-25 21:12:34 +0530

    Create FindAllDuplicates.java

---
+++ FindAllDuplicates.java
@@ -0,0 +1,20 @@
+//Problem Link: https://rb.gy/nofi8a
+
+//code
+class Solution 
+{
+    public List<Integer> findDuplicates(int[] nums) 
+    {
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) 
+        {
+            int num = Math.abs(nums[i]);
+            int idx = num - 1;
+            if (nums[idx] < 0)
+                result.add(num);
+            nums[idx] *= -1;
+        }
+        return result;
+    }
+}
