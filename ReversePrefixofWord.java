2f80b30c8ef9b1dfefd2f9ec25f754ac1a7a9dec
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-05-01 23:54:15 +0530

    Update ReversePrefixofWord.java

--- ReversePrefixofWord.java
+++ ReversePrefixofWord.java
@@ -1,1 +1,25 @@
 //Problem Link: https://rb.gy/o7c6xg
+
+//code
+class Solution 
+{
+    public String reversePrefix(String word, char ch) 
+    {
+        StringBuilder sb = new StringBuilder();
+        boolean flag = true;
+        for(int i = 0 ; i < word.length() ; i ++)
+        {
+            if(flag) 
+            {
+                sb.insert(0,word.charAt(i));
+            }
+            else
+            {
+                sb.append(word.charAt(i));
+            }
+            if(ch == word.charAt(i)) flag = false;
+        }    
+        if(flag) return word;
+        return sb.toString();
+    }
+}
